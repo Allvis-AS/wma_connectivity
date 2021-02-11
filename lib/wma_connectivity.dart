ad23ea66659645017254c315591004b0eea7501d
Sindre van der Linden <sindre@allvis.io>
2021-02-11 13:33:12 +0100

    Export dns_sensor.dart

--- lib/wma_connectivity.dart
+++ lib/wma_connectivity.dart
@@ -10,5 +10,5 @@ export 'package:wma_connectivity/src/sensor_event_types/api_sensor_event_type.da
 export 'package:wma_connectivity/src/sensor_event_types/dns_sensor_event_type.dart';
 export 'package:wma_connectivity/src/sensor_event_types/socket_exception_sensor_event_type.dart';
 export 'package:wma_connectivity/src/sensors/api_sensor.dart';
-export 'package:wma_connectivity/src/sensors/api_sensor.dart';
+export 'package:wma_connectivity/src/sensors/dns_sensor.dart';
 export 'package:wma_connectivity/src/wma_connectivity.dart';
